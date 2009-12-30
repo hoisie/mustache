19af205b2e3588c22b51d9a8550eeac7222862f9
Michael Hoisie <hoisie@scribd.com>
2009-12-30 14:29:28 -0800

    Added support for comments and meta-change tags

--- gostache_test.go
+++ gostache_test.go
@@ -14,15 +14,19 @@ var tests = []Test{
     Test{`hello {{name}}`, map[string]string{"name": "world"}, "hello world"},
     Test{`{{a}}{{b}}{{c}}{{d}}`, map[string]string{"a":"a","b":"b","c":"c","d":"d"}, "abcd"},
     Test{`0{{a}}1{{b}}23{{c}}456{{d}}89`, map[string]string{"a":"a","b":"b","c":"c","d":"d"}, "0a1b23c456d89"},
+    Test{`hello {{! comment }}world`, map[string]string{}, "hello world"},
+    Test{`{{ a }}{{=<% %>=}}<%b %><%={{ }}=%>{{ c }}`, map[string]string{"a":"a","b":"b","c":"c"}, "abc"},
+    Test{`{{ a }}{{= <% %> =}}<%b %><%= {{ }}=%>{{c}}`, map[string]string{"a":"a","b":"b","c":"c"}, "abc"},
 }
-
 
 func TestBasic(t *testing.T) {
 
     for _, test := range (tests) {
-        output := Render(test.tmpl, test.context)
-        if output != test.expected {
-            t.Fatalf("expected %q got %q", test.expected, output)
+        output,err := Render(test.tmpl, test.context)
+        if err != nil {
+          t.Fatalf("%q got error %q", test.tmpl, err.String())
+        } else if output != test.expected {
+            t.Fatalf("%q expected %q got %q", test.tmpl, test.expected, output)
         }
     }
 }
