16325702ab3d81ead8734f9b5f5255e27143f75f
kei10in <kei10in@gmail.com>
2017-11-28 06:13:03 +0900

    Remove padding arround triple mustache

--- mustache.go
+++ mustache.go
@@ -382,7 +382,8 @@ func (tmpl *Template) parseSection(section *sectionElement) error {
 		case '{':
 			if tag[len(tag)-1] == '}' {
 				//use a raw tag
-				section.elems = append(section.elems, &varElement{tag[1 : len(tag)-1], true})
+				name := strings.TrimSpace(tag[1 : len(tag)-1])
+				section.elems = append(section.elems, &varElement{name, true})
 			}
 		case '&':
 			name := strings.TrimSpace(tag[1:])
@@ -453,7 +454,8 @@ func (tmpl *Template) parse() error {
 		case '{':
 			//use a raw tag
 			if tag[len(tag)-1] == '}' {
-				tmpl.elems = append(tmpl.elems, &varElement{tag[1 : len(tag)-1], true})
+				name := strings.TrimSpace(tag[1 : len(tag)-1])
+				tmpl.elems = append(tmpl.elems, &varElement{name, true})
 			}
 		case '&':
 			name := strings.TrimSpace(tag[1:])
